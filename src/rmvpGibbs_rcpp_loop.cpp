ffd627dda6794f03ebeb9f9eeac887c98de6dd34
nicoaramayo <36645531+nicoaramayo@users.noreply.github.com>
2018-03-08 19:05:34 -0300

    Update rmvpGibbs_rcpp_loop.cpp

--- src/rmvpGibbs_rcpp_loop.cpp
+++ src/rmvpGibbs_rcpp_loop.cpp
@@ -13,6 +13,8 @@ vec drawwi_mvp(vec const& w, vec const& mu, mat const& sigmai, int p, ivec y,
   vec beta = betahat;
   
   vec outwi = w;
+	
+  int c = 1;
   
   for(int i = 0; i<ny; i++){	
 	  
@@ -28,14 +30,14 @@ vec drawwi_mvp(vec const& w, vec const& mu, mat const& sigmai, int p, ivec y,
 		// draw of the following choice
                   vec Cmout = condmom(outwi, mu, sigmai, p, i+1);
 		  //outwi[i] = rtrunSc(Cmout[0], Cmout[1], outwi[i-1], outwi[i+1]);
-		  outwi[i] = rtrunVec(Cmout[0], Cmout[1], outwi[i-1], outwi[i+1]);
+		  outwi[i] = rtrunVec(Cmout[0], Cmout[1], outwi[i-1], outwi[i+1], 1);
 		  
 	  } else if (y[i]<100){
 	  	// if it's one of i's observed responses (and it's the last observed response),
 		// sample from a truncated normal by the last w_i draw above and below from 0
                   vec Cmout = condmom(outwi, mu, sigmai, p, i+1);
 		  //outwi[i] = rtrunSc(Cmout[0], Cmout[1], outwi[i-1], 0.0);
-		  outwi[i] = rtrunVec(Cmout[0], Cmout[1], outwi[i-1], 0.0);
+		  outwi[i] = rtrunVec(Cmout[0], Cmout[1], outwi[i-1], 0.0, 1);
 		  
 	  } else {
 		// if it's a non-selected choice, sample from a negative truncated normal
