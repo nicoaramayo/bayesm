b2d593bf070bc21c5fc865f9b78a1275cdae27f7
nicoaramayo <36645531+nicoaramayo@users.noreply.github.com>
2018-03-13 15:55:18 -0300

    Update rmvpgibbs_rcpp.r

--- R/rmvpgibbs_rcpp.r
+++ R/rmvpgibbs_rcpp.r
@@ -1,6 +1,6 @@
 rmvpGibbs=function(Data,Prior,Mcmc){
 #
-# --------- EDITED TO COMPUTE THE MULTIVARIATE ORDERED PROBIT ---------------
+print(" --------- EDITED TO COMPUTE THE MULTIVARIATE ORDERED PROBIT ---------------")
  
 # Revision History:
 #   modified by rossi 12/18/04 to include error checking
