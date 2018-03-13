7e61730db51fbed25402c2e6b51c02d97d257a9a
nicoaramayo <36645531+nicoaramayo@users.noreply.github.com>
2018-03-13 11:57:18 -0300

    Update rmvpGibbs_rcpp_loop.cpp

--- src/rmvpGibbs_rcpp_loop.cpp
+++ src/rmvpGibbs_rcpp_loop.cpp
@@ -186,6 +186,6 @@ List rmvpGibbs_rcpp_loop(int R, int keep, int nprint, int p,
       
   return List::create(
     Named("betadraw") = betadraw, 
-    Named("sigmadraw") = sigmadraw,
+    //Named("sigmadraw") = sigmadraw,
     Named("wdraw") = wnew);
 }
