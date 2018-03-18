e8716dd4f46e2907b606edc36883bc1c0aa34138
nicoaramayo <36645531+nicoaramayo@users.noreply.github.com>
2018-03-18 17:23:55 -0300

    Update rmvpGibbs_rcpp_loop.cpp

--- src/rmvpGibbs_rcpp_loop.cpp
+++ src/rmvpGibbs_rcpp_loop.cpp
@@ -16,7 +16,7 @@ vec breg2(mat const& root, mat const& X, vec const& w, vec const& Abetabar) {
 
 // Output: draw from posterior
 
-// Model: y = Xbeta + e  e ~ N(0,I)
+// Model: w = Xbeta + e  e ~ N(0,I)
 
 // Prior: beta ~ N(betabar,A^-1)
 
@@ -197,10 +197,10 @@ List rmvpGibbs_rcpp_loop(int R, int keep, int nprint, int p,
   	for(int j=0; j<p; j++){
 		// for every observed response, sample in a ordered and uniform fashion between 0 and 1 the utility w
     		if(y[i*p + j] != 100){
-      			wnew[i*p + j] = 1 - (y[i*p + j]-1)/(double)suma;
+      			wnew[i*p + j] = (1 - (y[i*p + j]-1)/(double)suma)*50;
 		// for every not answered option, sample from negative uniform distribution between 0 and -1
     		}else{
-      			wnew[i*p + j] = runif(1, -1, 0)[0];}}
+      			wnew[i*p + j] = runif(1, -10, 0)[0];}}
 }
   
   //set initial values of w, beta, sigma (or root of inv)
