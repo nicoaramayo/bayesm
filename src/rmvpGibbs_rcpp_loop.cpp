46d58edeae0ea2f40ed673582ab30ab9d683915f
nicoaramayo <36645531+nicoaramayo@users.noreply.github.com>
2019-02-21 00:17:45 -0300

    Update rmvpGibbs_rcpp_loop.cpp

--- src/rmvpGibbs_rcpp_loop.cpp
+++ src/rmvpGibbs_rcpp_loop.cpp
@@ -198,7 +198,7 @@ vec first_order_demand(vec const& beta, mat const& X, mat const& sigmai){
 	
   for(int s = 0; s < p; s++){
   	for(int i = 0; i < X.n_rows/p; i++){
-		fo_demand[s] = fo_demand[s] + (exp(2*sqrt(2/pi)*dot(beta,X.row(i*p + s))/sigmai(s,s) * 
+		fo_demand[s] = fo_demand[s] + (exp(2*sqrt(2/pi)*dot(beta,X.row(i*p + s))/sigmai(s,s)) * 
 					       sigmai(s,s) * beta[4]*X(i*p + s, 4)) /
 			pow(1 + exp(2*sqrt(2/pi)*dot(beta,X.row(i*p + s))/sigmai(s,s)), 2);
   	}
