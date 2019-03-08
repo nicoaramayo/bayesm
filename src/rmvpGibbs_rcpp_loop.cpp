13855aae98bc8d09460c53b2c2be246edb398481
nicoaramayo <36645531+nicoaramayo@users.noreply.github.com>
2019-03-08 01:37:04 -0300

    Update rmvpGibbs_rcpp_loop.cpp

--- src/rmvpGibbs_rcpp_loop.cpp
+++ src/rmvpGibbs_rcpp_loop.cpp
@@ -170,7 +170,7 @@ vec price_density_s(vec const& sigma_s, vec const& price_s, vec const& fo_demand
   double pi = 3.1415926;
 
   for(int s=0; s<p; s++){
-	price_density[s] = 1/(sqrt(2*pi*sigma_s[1]));
+	price_density[s] = 1/(sqrt(2*pi*sigma_s[1]))*exp(-1/(2*sigma_s[1])*(log(price_s[s] + pow(fo_demand_s[s], -1));
       	//price_density[s] = 1/(sqrt(2*pi*sigma_s[1]))*exp(-1/(2*sigma_s[1])*(log(price_s[s] + pow(fo_demand_s[s], -1)*demand_s[s]))
 	//					 - gamma*z_s[s])*eps(fo_cost_s[s]);
 						 //- dot(gamma,z_s[s]))*eps(fo_cost_s[s]);
